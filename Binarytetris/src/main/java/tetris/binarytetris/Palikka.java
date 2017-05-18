ba9afe2916bc91bcf1d56d9198657417e6236f15
vvenla <venla.viljamaa@helsinki.fi>
2017-05-18 13:56:26 +0300

    Projektin alustusta

---
+++ Binarytetris/src/main/java/tetris/binarytetris/Palikka.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetris.binarytetris;
+
+/**
+ *
+ * @author Venla Viljamaa
+ */
+public class Palikka {
+    
+    private int arvo;
+
+    public Palikka(int arvo) {
+        this.arvo = arvo;
+    }
+
+    
+    public Palikka() {
+    }
+
+    public int getArvo() {
+        return arvo;
+    }
+
+    public void setArvo(int arvo) {
+        this.arvo = arvo;
+    }
+    
+    
+    
+}
---
+++ dokumentaatio/tuntikirjanpito.md
@@ -0,0 +1,6 @@
+### Tuntikirjanpito
+Päivä | Tunnit | Kuvaus
+--------------- | ----- | ------
+18.05.2017 | 1.5h | Projektin luomista
+
+... | ... | ...
